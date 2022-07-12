6487f305b14b227241a91302f73b65d9aaef37c7
Tom Van den Bulck <tom.vandenbulck@ordina.be>
2022-07-12 16:47:27 +0200

    first commit

---
+++ src/test/java/com/ordina/eventing/EventingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordina.eventing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
